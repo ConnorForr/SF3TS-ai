983f58c47cf11c0d4523c09c8f12c8a8eb8454e5
Connor <connor.forristal@gmail.com>
2023-11-16 10:29:38 -0600

    Start of External Tool For BizHawk

---
+++ Sockets/src/SocketsForm.cs
@@ -0,0 +1,15 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+using BizHawk.Client.Common;
+using BizHawk.Client.EmuHawk;
+
+
+namespace Net.MyStuff.Sockets
+{
+    [ExternalTool("Sockets")] // this appears in the Tools > External Tools submenu in EmuHawk
+    public sealed class SocketsTool : ToolFormBase, IExternalToolForm
+    {
+        
+    }
+}
